0489c8de39f6f8b3145b9678bce64542e5d74ccc
auddus16 <auddus16@naver.com>
2021-10-18 19:51:17 +0900

    HwangMyongYeon-Controller+

--- src/main/java/com/paas/runup/controller/ClassController.java
+++ src/main/java/com/paas/runup/controller/ClassController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.paas.runup.dto.AttendDTO;
 import com.paas.runup.dto.ClassDTO;
 import com.paas.runup.dto.RegisterDTO;
+import com.paas.runup.email.EmailUtil;
 import com.paas.runup.service.AttendService;
 import com.paas.runup.service.ClassService;
 import com.paas.runup.service.RegisterService;
@@ -30,7 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value="/class")
-public class ClassController {
+public class ClassController { //선생님, 학생 - 수업 관리 (수업, 출석, 등록)
 	
 	@Autowired
 	private ClassService classService;
@@ -40,6 +41,9 @@ public class ClassController {
 	
 	@Autowired
 	private AttendService attendService;
+	
+	@Autowired
+	private EmailUtil emailUtil;
 	
 	@ResponseBody
 	
@@ -81,6 +85,50 @@ public class ClassController {
 		return attendService.selectAttendByDate(hm);
 	}
 	
+	@ApiOperation(value= "선생님 - 학생 출결 정보 수정", notes= "선생님이 학생 출결 정보를 수정한다.")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name="a_no", value="출결 번호", dataType = "int", example= "1"),
+		@ApiImplicitParam(name="a_state", value="출결 상태(0:출석 1:지각 2:결석)", dataType = "int", example= "1")
+	})
+	@RequestMapping(value= "/teacher/attend/{a_no}/{a_state}", method= RequestMethod.PUT)
+	public boolean modifyAttend(@PathVariable int a_no, @PathVariable int a_state) throws Exception {
+		
+		System.out.println("학생 출결 정보 수정 메소드 시작");
+		
+		if(attendService.updateAttendState(a_no, a_state)==0) {
+			System.out.println("학생 출결 정보 수정 실패!!");
+			return false;
+		}
+		
+		System.out.println("학생 출결 정보 수정 완료");
+
+		return true;
+
+	}
+	
+	@ApiOperation(value= "선생님 - 수업에서 해당 학생 삭제", notes="선생님이 수업에서 학생을 삭제한다.")
+	@RequestMapping(value= "/teacher/attend/del", method= RequestMethod.DELETE)
+	public boolean deleteRegister(@RequestBody RegisterDTO r) throws Exception {
+		
+		System.out.println("등록 삭제 메소드 시작");
+		
+		//등록 테이블 삭제
+		if(registerService.deleteRegister(r)!=0) {
+			//수업 테이블 학생 수 감소
+			if(classService.subStudent(r.getC_no())==0) {
+				System.out.println("수업 학생 수 감소 실패!!");
+			}
+			System.out.println("수업 삭제 완료");
+			
+			return true;
+			
+		}
+		
+		System.out.println("수업 삭제 실패!!");
+		
+		return false;
+	}
+	
 	@ApiOperation(value= "선생님 - 새로운 수업 추가", notes="선생님이 새로운 수업을 생성한다.")
 	@RequestMapping(value = "/teacher/new", method = RequestMethod.POST)
 	public boolean addNewClass(@RequestBody ClassDTO c) throws Exception {
@@ -98,6 +146,55 @@ public class ClassController {
 		System.out.println("새로운 수업 추가 완료");
 		
 		return true;
+	}
+	
+	@ApiOperation(value= "선생님 - 학생 초대", notes="선생님이 수업에 학생을 초대한다.")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name="emailMap", value="이메일 리스트", dataType = "Object", example= "{\r\n" + 
+				"  \"email\" : [\"email1@naver.com\", \"email2@naver.com\"]\r\n" + 
+				"}"),
+		@ApiImplicitParam(name="c_name", value="수업 이름", dataType = "String", example= "수학1반")
+	})
+	@RequestMapping(value= "/teacher/email/{c_name}", method = RequestMethod.POST)
+	public boolean saveLocation(@RequestBody HashMap<String, List<Object>> emailMap, @PathVariable String c_name) {
+		System.out.println("학생 초대 메소드 시작");
+		
+		List<Object> emailList= new ArrayList<Object>();
+		emailList= emailMap.get("email");
+		
+		//++선생님 이름(로그인 정보), 수업 정보를 이메일에 함께 넣어보자.
+		//학생이 존재하나요??-굳이 해야하나?
+		for(Object s : emailList) {
+			emailUtil.sendEmail((String)s, "RunUpTool 수업 초대", "초대 테스트 메일 입니다.");
+			System.out.println((String)s+"초대합니다.");
+		}
+		
+		return true;
+	}
+	
+	@ApiOperation(value="학생 수업 등록", notes="수업에 학생을 등록시킨다.")
+	@RequestMapping(value= "/register", method= RequestMethod.POST)
+	public boolean registerStudent(@RequestBody RegisterDTO r) throws Exception{
+		
+		System.out.println("학생 수업 등록 조회 메소드 시작");
+		//등록 테이블 삽입
+		if(registerService.insertRegister(r)!=0) {
+			
+			System.out.println("수업 학생 수 증가 메소드 시작");
+			//수업 테이블 학생 수 증가
+			if(classService.addStudent(r.getC_no())==0) {
+				System.out.println("수업 학생 수 증가 실패!!");
+			}
+			
+			System.out.println("학생 수업 등록 성공");
+			
+			return true;
+		}
+		
+		System.out.println("학생 수업 등록 실패!!");
+		
+		return false;
+		
 	}
 	
 	@ApiOperation(value= "선생님 - 수업 정보 수정", notes= "선생님이 수업 정보를 수정한다.")
